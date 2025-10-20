66556186e19c1e49bc030975a17716ed72ee4b03
Rupam Ghosh <117834750+hul0@users.noreply.github.com>
2025-10-20 13:19:08 +0000

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://hul0.github.io/",
-	base: "/hulo-blogs",
+	site: "https://blog.hulobiral.online/",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
